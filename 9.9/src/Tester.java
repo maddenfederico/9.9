01cba6c8f20dc0276c7550beaeafcb0ed7b6a333
Federico Madden <maddenfederico@gmail.com>
2017-03-12 21:45:16 -0700

    commit

--- 9.9/src/Tester.java
+++ 9.9/src/Tester.java
@@ -3,6 +3,14 @@ public class Tester
 {
 	public static void main(String[] args)
 	{
+		Employee employee1 = new Employee("Jason", 40000);
+		Manager manager1 = new Manager("John", 100000, "Informatics");
+		Executive exec1 = new Executive("Ben", 1000000, "HR");
+		
+		System.out.println(employee1);
+		System.out.println(manager1);
+		System.out.println(exec1);
+		
 		
 	}
 }
